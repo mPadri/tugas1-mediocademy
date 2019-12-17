f54d6a40b06de8f201f6d37490ffbf715fbd8ea1
mPadri <cvadhri@gmail.com>
2019-12-17 17:50:23 +0700

    tugas object

---
+++ script.js
@@ -0,0 +1,45 @@
+// Nomer 1
+var kendaraan = ['Motor','Kapal','Pesawat','Sepeda','Otopad'];
+
+// Nomer 2
+var Motor={
+	color: 'blue',
+	roda : 2,
+	year : 2019,
+	maju : function (){return 'maju'},
+	mundur: function (){return 'mundur'}
+
+}
+
+var Kapal={
+	color : 'white',
+	jenis : 'speed boat',
+	year  : 2001,
+	maju : function (){return 'maju'},
+	mundur: function (){return 'mundur'}
+}
+
+var Pesawat={
+	merk : 'Garuda',
+	color : 'white',
+	year : 2000,
+	terbang : function (){return 'terbang'},
+	mendarat: function (){return 'mendarat'}
+}
+
+var Sepeda={
+	color: 'black',
+	jenis : 'BMX',
+	year : 2018,
+	maju : function (){return 'maju'},
+	mundur: function (){return 'mundur'}
+}
+
+var Otopad={
+	color: 'Green',
+	jenis: 'sekuter listrik',
+	year : 2017,
+	maju : function (){return 'maju'},
+	mundur: function (){return 'mundur'}
+
+}
